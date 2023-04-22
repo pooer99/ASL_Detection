83b763063434b2dd32c69c21247c7752d5f93677
pooer <488998906@qq.com>
2023-04-22 21:42:08 +0800

    添加模糊、随机缩放方法；添加清空输出的文件夹功能

--- Scripts/augmentor.py
+++ Scripts/augmentor.py
@@ -1,3 +1,5 @@
+import shutil
+
 import albumentations as A
 import cv2
 import os
@@ -14,16 +16,30 @@ def make_augmentor():
     random.seed(42)
 
     # 以下是各种图像处理的增强管道的定义：
-    # 水平翻转
-    augmentors = {'HorizontalFlip': A.Compose([
-        A.HorizontalFlip(p=1)
-    ], bbox_params=bbox_param)}
+    augmentor = {
 
-    return augmentors
+        # 水平翻转
+        'HorizontalFlip': A.Compose([
+        A.HorizontalFlip(p=1)
+    ], bbox_params=bbox_param),
+
+        # 模糊
+        'Blur': A.Compose([
+            A.Blur(p=1, always_apply= True)
+        ], bbox_params=bbox_param),
+
+        # 随机缩放
+        'RandomScale': A.Compose([
+            A.RandomScale(scale_limit=0.5, always_apply= True),
+        ], bbox_params=bbox_param),
+
+    }
+
+    return augmentor
 
 
 '''获取图片'''
-def augment_images(transform):
+def augment_images(transform, opt):
 
     # 文件根目录
     dataset_path = r'F:\augment_image_test\input'
@@ -82,6 +98,10 @@ def augment_images(transform):
 
         # 保存图片，标签
         transformed_image = cv2.cvtColor(transformed_image,cv2.COLOR_BGR2RGB)
+
+        files_name[i] = opt + '_' + files_name[i]
+        file_label_name = opt + '_' + file_label_name
+
         cv2.imwrite(os.path.join(output_images_path, files_name[i]), transformed_image)
         write_bb(os.path.join(output_labels_path, file_label_name), transformed_bboxes, ids)
 
@@ -93,12 +113,40 @@ def show_iamges_with_bboxes(image,bbox):
     result = visualize_bbox(image,bbox)
     visualize(result)
 
+'''删除测试数据'''
+def delet_images():
+    output_images_path = r'F:\augment_image_test\out\images'
+    output_labels_path = r'F:\augment_image_test\out\labels'
+
+    # 清空文件夹
+    shutil.rmtree(output_images_path)
+    os.mkdir(output_images_path)
+
+    # 重新生成文件夹
+    shutil.rmtree(output_labels_path)
+    os.mkdir(output_labels_path)
+
+    # 复制classes.txt
+    shutil.copy(r'F:\augment_image_test\input\labels\classes.txt', output_labels_path)
+
 if __name__ == '__main__':
+
+    # 清空输出文件夹
+    #delet_images()
+
     # 获取增强管道
     augmentors = make_augmentor()
 
     # 进行图片增强
-    augment_images(augmentors['HorizontalFlip'])
+    '''
+    增强方法 opt :
+        HorizontalFlip : 水平翻转
+        Blur           : 模糊
+        RandomScale    : 随机缩放
+    '''
+
+    opt = 'RandomScale' # 选择增强的方法
+    augment_images(augmentors[opt], opt)
 
 
 
